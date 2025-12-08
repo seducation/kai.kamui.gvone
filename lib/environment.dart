c8f479e322a9172e9ad57d49921fe69a10a12b10
educationrb76 <educationrb76@gmail.com>
2025-12-08 11:45:09 +0000

    photo fixes

--- lib/environment.dart
+++ lib/environment.dart
@@ -2,5 +2,5 @@ class Environment {
   static const String appwriteProjectId = '692fb6d40039dc8a248d';
   static const String appwritePublicEndpoint = 'https://fra.cloud.appwrite.io/v1';
   static const String appwriteDatabaseId = '692fb6ff0039d5bd19af';
-  static const String appwriteStorageBucketId = '692e841600305867b999';
+  static const String appwriteStorageBucketId = 'lens-s';
 }
